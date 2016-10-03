42d88016c6a2b0ee1045295a205ca4de5439e953
meg768 <magnus@egelberg.se>
2016-10-03 19:22:12 +0200

    -

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 
-BUCKET=gs://app-o
-DATESTAMP=$(date +"%Y.%m.%d.%H.%M")
-BACKUP_FILE=mysql.${DATESTAMP}.sql.gz
-BUCKET_PATH=mysql/backups
-
 FILENAME=$(basename "$0")
 LOCATION="$(cd "$(dirname "$0")" && pwd)"
+DATESTAMP=$(date +"%Y-%m-%d-%H-%M")
+
+BACKUP_NAME=mysql-backup-${DATESTAMP}.sql.gz
+
+SRC=${LOCATION}/${BACKUP_NAME}
+DST=gs://app-o/mysql/backups/${BACKUP_NAME}
 
 #exec >> ${LOCATION}/logs/${FILENAME%.*}.log
 #exec 2>&1
 
-cd ${LOCATION}/../backups
+cd ${LOCATION}
 
-echo Backing up database to ${BACKUP_FILE}...
-mysqldump --triggers --routines --quick --user root -ppotatismos munch | gzip >  ${BACKUP_FILE}
+echo Backing up database to ${SRC}...
+mysqldump --triggers --routines --quick --user root -ppotatismos munch | gzip > ${SRC}
 
-echo Uploading ${BACKUP_FILE} to ${BUCKET}/${BUCKET_PATH}/${BACKUP_FILE}...
-gsutil cp ${BACKUP_FILE}  ${BUCKET}/${BUCKET_PATH}/${BACKUP_FILE}
+echo Uploading ${SRC} to ${DST}...
+gsutil cp ${SRC} ${DST}
 
 echo Cleaning up...
-#rm ${BACKUP_FILE}
+#rm ${SRC}
 
 echo Done.
