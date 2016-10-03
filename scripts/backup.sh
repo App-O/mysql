9d542b19128a7399e48c976af08577ca0b30aa19
meg768 <magnus@egelberg.se>
2016-10-03 18:55:43 +0200

    -

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,15 +4,14 @@ BUCKET=gs://app-o
 DATESTAMP=$(date +"%Y.%m.%d.%H.%M")
 BACKUP_FILE=mysql.${DATESTAMP}.sql.gz
 BUCKET_PATH=mysql/backups
-HOME=/home/app_o/mysql/backups
 
 FILENAME=$(basename "$0")
 LOCATION="$(cd "$(dirname "$0")" && pwd)"
 
-exec >> ${LOCATION}/logs/${FILENAME%.*}.log
-exec 2>&1
+#exec >> ${LOCATION}/logs/${FILENAME%.*}.log
+#exec 2>&1
 
-cd ${HOME}
+cd ${LOCATION}/../backups
 
 echo Backing up database to ${BACKUP_FILE}...
 mysqldump --triggers --routines --quick --user root -ppotatismos munch | gzip >  ${BACKUP_FILE}
@@ -21,6 +20,6 @@ echo Uploading ${BACKUP_FILE} to ${BUCKET}/${BUCKET_PATH}/${BACKUP_FILE}...
 gsutil cp ${BACKUP_FILE}  ${BUCKET}/${BUCKET_PATH}/${BACKUP_FILE}
 
 echo Cleaning up...
-rm ${BACKUP_FILE}
+#rm ${BACKUP_FILE}
 
 echo Done.
